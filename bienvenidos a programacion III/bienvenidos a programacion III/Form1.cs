3ed4dfd0cc89eddc84ce1754d94c250ca2cebaba
migueluapa <33141771+migueluapa@users.noreply.github.com>
2017-11-02 01:43:08 -0400

    Bienvenidos a Programacion

---
+++ bienvenidos a programacion III/bienvenidos a programacion III/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bienvenidos_a_programacion_III
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtmensaje.Text = "Bienvenidos a Programacion III";
+        }
+    }
+}
